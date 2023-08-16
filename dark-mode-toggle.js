9394c60ed6deb5ea446c6d963d4a43ddd232a34f
cschiappa <cristina.pietra@hotmail.com>
2023-08-16 14:09:37 +0100

    Created an automating dark/light theme switch

--- dark-mode-toggle.js
+++ dark-mode-toggle.js
@@ -1,20 +1,15 @@
 const darkModeToggle = document.getElementById("darkModeToggle");
-const lightStyles = document.getElementById("lightStyles");
-const darkStyles = document.getElementById("darkStyles");
-const body = document.body;
-const textDark = document.getElementById("textDark")
+const textDark = document.getElementById("textDark");
+const html = document.documentElement;
+
 
 darkModeToggle.addEventListener("change", () => {
     if (darkModeToggle.checked) {
-        lightStyles.disabled = true;
-        darkStyles.disabled = false;
-        body.classList.add("dark-mode-style");
         textDark.innerHTML = "darker";
+        html.setAttribute("data-theme", "light");
 
     } else {
-        lightStyles.disabled = false;
-        darkStyles.disabled = true;
-        body.classList.remove("dark-mode-style");
         textDark.innerHTML = "lighter";
+        html.setAttribute("data-theme", "dark");
     }
 });
